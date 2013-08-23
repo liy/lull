b80dbdbecf03bec125e2428f8dbb97ab16f76eb9
liy <liy8272@gmail.com>
2013-08-23 16:58:29 +0100

    update

--- js/display/Scene.js
+++ js/display/Scene.js
@@ -9,10 +9,19 @@ function Scene(w, h){
   background.graphics.endFill();
   this.addChild(background);
 
-  this.addEventListener('mouseup', this.onClick);
+  this.addEventListener('mouseup', bind(this, this.onClick));
 }
 var p = Scene.prototype = Object.create(Container.prototype);
 
 p.onClick = function(e){
-  console.log(e.target);
+  console.log(e.currentTarget.width);
+  this.onDraw = this.zoomIn;
+}
+
+p.zoomIn = function(){
+  // console.log('zoom in');
+  var dw = (1024*0.8 - this.width)/20;
+  var dh = (768*0.8 - this.height)/20;
+  this.width += dw/5;
+  this.height += dh/5;
 }

9f16e44dc165c5e2475e723363439688c51b4bc8
liy <liy8272@gmail.com>
2013-09-05 16:50:07 +0100

    Remove old files

--- js/rest/WebService.js
+++
@@ -1,9 +0,0 @@
-function WebService(endPoint){
-  this.endPoint = endPoint.charAt(endPoint.length-1) === '/' ? endPoint : endPoint + '/';
-}
-var p = WebService.prototype;
-
-p.getOperation = function(path, method){
-  var p = path.charAt(0) === '/' ? path.substring(1) : path;
-  return new Operation(this.endPoint + p, method);
-}
